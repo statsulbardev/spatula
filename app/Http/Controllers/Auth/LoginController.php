0fd76666026b17449779f203c9c68ff7c789106b
Syaifur Rijal <syaifur.rijal@hotmail.com>
2020-05-06 20:55:36 +0800

    Ganti menjadi otentikasi native laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-// use Statsulbar\BPSAuth\Facades\BPSAuth;
-use App\Utility\Authentication as Otentikasi;
 
 class LoginController extends Controller
 {
@@ -45,45 +40,5 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
-    }
-
-    public function showLoginForm()
-    {
-        return view('auth.login');
-    }
-
-    public function login(Otentikasi $auth, Request $request)
-    {
-        $this->validateLogin($request);
-
-        // if(Auth::loginUsingId(1)) return $this->sendLoginResponse($request);
-
-        if($auth->login($request->username, $request->password)) {
-            $bps_id = $auth->getBpsId();
-
-            $profile = $auth->getProfil($bps_id);
-
-            $user = User::where('bps_id', $bps_id)->first();
-
-            if(!empty($user)) {
-                try {
-                    $user->update([
-                        'name'        => $profile['nama'],
-                        'email'       => $profile['email'],
-                        'bps_id'      => $bps_id,
-                        'employee_id' => $profile['nippanjang'],
-                        'photo_path'  => $profile['urlfoto'],
-                    ]);
-                } catch(\Exception $e) {
-                    return abort(500);
-                }
-
-                if(Auth::loginUsingId($user->id)) return $this->sendLoginResponse($request);
-            } else {
-                return abort(403);
-            }
-        } else {
-            return abort(401);
-        }
     }
 }
