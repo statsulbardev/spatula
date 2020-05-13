e189351097f92812eedd515f534af6cbf3150c06
Syaifur Rijal <syaifur.rijal@hotmail.com>
2020-05-13 12:01:11 +0800

    Update untuk postgresql

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,9 +10,15 @@ class ReportController extends Controller
 {
     public function monthly()
     {
-        // $result = d_penilaian::select(DB::Raw('YEAR(created_at) as year'))->distinct()->get(); -> untuk mysql
-        $years = d_penilaian::select(DB::Raw('EXTRACT(year from created_at)'))->distinct()->get();
-        // $years  = $result->pluck('year');
+        /**
+         * Untuk MySQL
+         *
+         * $result = d_penilaian::select(DB::Raw('YEAR(created_at) as year'))->distinct()->get();
+         * $years  = $result->pluck('year');
+         */
+
+        $result = d_penilaian::select(DB::Raw('EXTRACT(year from created_at)'))->distinct()->get();
+        $years  = $result->pluck('date_part');
 
         $col3 = collect([]);
 
@@ -21,9 +27,16 @@ class ReportController extends Controller
 
     public function daily()
     {
-        // $result = d_penilaian::select(DB::Raw('YEAR(created_at) as year'))->distinct()->get(); -> untuk mysql
+        /**
+         * Untuk MySQL
+         *
+         * $result = d_penilaian::select(DB::Raw('YEAR(created_at) as year'))->distinct()->get();
+         * $years  = $result->pluck('year');
+         */
+
         $result = d_penilaian::select(DB::Raw('EXTRACT(year from created_at)'))->distinct()->get();
-        $years  = $result->pluck('year');
+        $years  = $result->pluck('date_part');
+
         $data   = d_penilaian::where('selesai', 1)->orderBy('created_at', 'desc')->paginate(15);
 
         return view('backend.report.daily', compact('years', 'data'));
@@ -31,9 +44,15 @@ class ReportController extends Controller
 
     public function showDailyDetail(Request $request)
     {
-        // $result = d_penilaian::select(DB::Raw('YEAR(created_at) as year'))->distinct()->get(); -> untuk mysql
+        /**
+         * Untuk MySQL
+         *
+         * $result = d_penilaian::select(DB::Raw('YEAR(created_at) as year'))->distinct()->get();
+         * $years  = $result->pluck('year');
+         */
+
         $result = d_penilaian::select(DB::Raw('EXTRACT(year from created_at)'))->distinct()->get();
-        $years  = $result->pluck('year');
+        $years  = $result->pluck('date_part');
 
         $data = d_penilaian::whereYear('created_at', '=', $request->tahun)
                 ->whereMonth('created_at', '=', $request->bulan)
