51489725f36c16b7ea2d48c45bad03ba1b6f8a5e
Syaifur Rijal <syaifur.rijal@hotmail.com>
2020-06-10 20:51:26 +0800

    Ganti teks interface

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,9 +12,9 @@
                     <div class="info d-flex align-items-center">
                         <div class="content">
                             <div class="logo">
-                                <h1>Dashboard</h1>
+                                <h1>SPATULA</h1>
                             </div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p>Saran Pengaduan Online dan Rating Pelayanan Petugas</p>
                         </div>
                     </div>
                 </div>
