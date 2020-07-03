730e4ebcc0f8f8631a695847c08f1f64bada255a
Syaifur Rijal <syaifur.rijal@hotmail.com>
2020-07-03 11:30:38 +0800

    Tambah ViewerJS

--- resources/views/backend/panduan/index.blade.php
+++ resources/views/backend/panduan/index.blade.php
@@ -13,28 +13,7 @@
     <!-- Dashboard Counts Section-->
     <section class="dashboard-counts no-padding-bottom">
         <div class="container-fluid text-center">
-            <canvas class="border rounded shadow" id="pdf_canvas"></canvas>
+            <iframe src="{{ asset('public/vendor/ViewerJS/#../../files/pdf/panduan.pdf') }}" class="w-100 p-1 border rounded shadow mb-5" style="display:block;height:100vh;" allowfullscreen webkitallowfullscreen></iframe>
         </div>
       </section>
 @endsection
-
-@push('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/pdfjs-dist@2.0.943/build/pdf.min.js"></script>
-    <script>
-        pdfjsLib.getDocument("{{ asset('public/files/pdf/laravel.pdf') }}").then(doc => {
-            doc.getPage(1).then(page => {
-                let myCanvas = document.getElementById("pdf_canvas");
-                let context  = myCanvas.getContext("2d");
-                let viewport = page.getViewport(2);
-
-                myCanvas.width  = viewport.width;
-                myCanvas.height = viewport.height;
-
-                page.render({
-                    canvasContext: context,
-                    viewport: viewport
-                });
-            });
-        });
-    </script>
-@endpush
