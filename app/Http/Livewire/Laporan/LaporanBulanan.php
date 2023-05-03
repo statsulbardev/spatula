dd912c892691202f032578b170f960fce290d1f5
Syaifur Rijal <syaifur.rijal@hotmail.com>
2023-05-03 15:50:22 +0800

    Update Laporan Bulanan

--- app/Http/Livewire/Laporan/LaporanBulanan.php
+++ app/Http/Livewire/Laporan/LaporanBulanan.php
@@ -13,7 +13,9 @@ class LaporanBulanan extends Component
 {
     use UnitCode, WithPagination;
 
-    public $monthlyReport;
+    public $officerRating;
+    public $serviceRating;
+    public $complaintSuggestion;
     public $years;
     public $selectedYear;
     public $selectedReport;
@@ -30,7 +32,13 @@ class LaporanBulanan extends Component
 
         $this->getYears();
 
-        $this->monthlyReport = $this->getOfficerRating($this->selectedYear, Auth::user()->hasRole('superadmin'));
+        dd($this->getComplaintSuggestion(Auth::user()->hasRole('superadmin')));
+
+        $this->officerRating = $this->getOfficerRating(Auth::user()->hasRole('superadmin'));
+
+        $this->serviceRating = $this->getServiceRating(Auth::user()->hasRole('superadmin'));
+
+        $this->complaintSuggestion = $this->getComplaintSuggestion(Auth::user()->hasRole('superadmin'));
     }
 
     private function getYears()
@@ -39,16 +47,43 @@ class LaporanBulanan extends Component
         $this->years  = $result->pluck('year');
     }
 
-    private function getOfficerRating($year, $role)
+    private function getOfficerRating($role)
     {
         if ($role) {
-            $result = DB::select('SELECT MONTH(a.created_at) as bulan, b.nama , AVG(rating_petugas) as rerata, COUNT(rating_petugas) as jumlah_terlayani
-                    FROM d_penilaian a, m_pengguna b WHERE a.kode_petugas=b.id and YEAR(a.created_at) = ' . $year . ' GROUP BY MONTH(a.created_at), kode_petugas');
+            $result = DB::select(
+                        'SELECT
+                            MONTH(a.created_at) as bulan,
+                            b.nama,
+                            AVG(a.rating_petugas) as rerata,
+                            COUNT(a.rating_petugas) as jumlah_terlayani
+                        FROM
+                            d_penilaian a,
+                            m_pengguna b
+                        WHERE
+                            a.kode_petugas = b.id AND
+                            YEAR(a.created_at) = ' . $this->selectedYear . '
+                        GROUP BY
+                            MONTH(a.created_at),
+                            kode_petugas'
+                        );
         } else {
-            $result = DB::select('SELECT MONTH(a.created_at) as bulan, b.nama , AVG(rating_petugas) as rerata, COUNT(rating_petugas) as jumlah_terlayani
-                    FROM d_penilaian a, m_pengguna b
-                    WHERE a.kode_satker_id = ' . $this->getUnitCode()->kode_satker . ' AND a.kode_petugas=b.id AND YEAR(a.created_at) = '. $this->selectedYear .'
-                    GROUP BY a.kode_satker_id, MONTH(a.created_at), kode_petugas');
+            $result = DB::select(
+                        'SELECT MONTH(a.created_at) as bulan,
+                            b.nama,
+                            AVG(rating_petugas) as rerata,
+                            COUNT(rating_petugas) as jumlah_terlayani
+                        FROM
+                            d_penilaian a,
+                            m_pengguna b
+                        WHERE
+                            a.kode_satker_id = ' . $this->getUnitCode()->kode_satker . ' AND
+                            a.kode_petugas=b.id AND
+                            YEAR(a.created_at) = '. $this->selectedYear .'
+                        GROUP BY
+                            a.kode_satker_id,
+                            MONTH(a.created_at),
+                            kode_petugas'
+                    );
         }
 
         $column = ['Bulan', 'Nama Petugas', 'Rating Rata-Rata', 'Jumlah Penilaian'];
@@ -58,26 +93,52 @@ class LaporanBulanan extends Component
 
     private function getServiceRating($role)
     {
-        if ($role === 'superadmin') {
-            $result = DB::select('SELECT MONTH(a.created_at) as bulan, b.nama_layanan , AVG(rating_layanan) as rerata, COUNT(rating_layanan) as jumlah_terlayani
-                    FROM d_penilaian a, m_layanan b
-                    WHERE a.kode_layanan=b.kode_layanan AND YEAR(a.created_at) = '. $this->selectedYear .' GROUP BY MONTH(a.created_at), a.kode_layanan');
+        if ($role) {
+            $result = DB::select(
+                        'SELECT
+                            MONTH(a.created_at) as bulan,
+                            b.nama_layanan,
+                            AVG(a.rating_layanan) as rerata,
+                            COUNT(a.rating_layanan) as jumlah_terlayani
+                        FROM
+                            d_penilaian a,
+                            m_layanan b
+                        WHERE
+                            a.kode_layanan = b.kode_layanan AND
+                            YEAR(a.created_at) = '. $this->selectedYear .'
+                        GROUP BY
+                            MONTH(a.created_at),
+                            b.nama_layanan'
+                        );
         } else {
             $result = DB::select('SELECT MONTH(a.created_at) as bulan, b.nama_layanan , AVG(rating_layanan) as rerata, COUNT(rating_layanan) as jumlah_terlayani
                     FROM d_penilaian a, m_layanan b WHERE a.kode_satker_id = ' . $this->getUnitCode()->kode_satker . ' AND a.kode_layanan=b.kode_layanan AND YEAR(a.created_at) = '. $this->selectedYear .'
                     GROUP BY a.kode_satker_id, MONTH(a.created_at), a.kode_layanan');
         }
 
-        return $result;
+        $column = ['Bulan', 'Nama Layanan', 'Rating Rata-Rata', 'Jumlah Penilaian'];
+
+        return [$column, $result];
     }
 
     private function getComplaintSuggestion($role)
     {
-        if ($role === 'superadmin') {
-            $result = DB::select("SELECT MONTH(a.created_at) as bulan, b.nama_saran , COUNT(a.kode_saran) as jumlah_saran
-                    FROM d_penilaian a, m_saran b
-                    WHERE YEAR(a.created_at) = '. $this->selectedYear .' AND a.kode_saran LIKE CONCAT('%', b.kode_saran ,'%')
-                    GROUP BY YEAR(a.created_at), MONTH(a.created_at), b.kode_saran");
+        if ($role) {
+            $result = DB::select(
+                        "SELECT
+                            MONTH(a.created_at) as bulan,
+                            b.nama_saran,
+                            COUNT(a.kode_saran) as jumlah_saran
+                        FROM
+                            d_penilaian a,
+                            m_saran b
+                        WHERE
+                            YEAR(a.created_at) = '. $this->selectedYear .' AND
+                            a.kode_saran LIKE CONCAT('%', b.kode_saran ,'%')
+                        GROUP BY
+                            MONTH(a.created_at),
+                            b.nama_saran"
+                        );
         } else {
             $result = DB::select("SELECT MONTH(a.created_at) as bulan, b.nama_saran , COUNT(a.kode_saran) as jumlah_saran
                     FROM d_penilaian a, m_saran b WHERE a.kode_satker_id = " . $this->getUnitCode()->kode_satker . " AND YEAR(a.created_at) = '. $this->selectedYear .' AND a.kode_saran LIKE CONCAT('%', b.kode_saran ,'%')
--- resources/views/livewire/laporan/laporan-bulanan.blade.php
+++ resources/views/livewire/laporan/laporan-bulanan.blade.php
@@ -2,22 +2,53 @@
 
 <div>
     {{-- Header --}}
-    @include('components.page.page-title', ['title' => 'Laporan Bulanan'])
+    @include('components.page.page-title', ['title' => 'Laporan Bulanan Tahun ' . $selectedYear])
 
     <section class="mt-10 mb-6">
         <div class="w-full bg-white rounded shadow overflow-x-auto">
-            <div class="p-4 flex flex-wrap items-center justify-between">
-                <div wire:ignore>
-                    <select wire:model="selectedReport" data-te-select-init data-te-select-filter="true" data-te-select-size="lg">
-                        <option value="null" hidden selected>Pilih Laporan ...</option>
-                        <option value="1">Rating Petugas Layanan</option>
-                        <option value="2">Rating Layanan</option>
-                        <option value="3">Saran Pengaduan</option>
-                    </select>
-                    <label data-te-select-label-ref>Jenis Laporan</label>
-                </div>
-                <div class="flex flex-wrap">
-                    <div wire:ignore>
+            <div class="items-center justify-between">
+                {{-- Tab --}}
+                <div class="px-4 flex flex-wrap items-center justify-between">
+                    <ul class="mb-5 flex list-none flex-row flex-wrap border-b-0 pl-0" role="tablist" data-te-nav-ref>
+                        <li role="presentation">
+                            <a
+                                href="#rating-petugas-layanan"
+                                class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+                                data-te-toggle="pill"
+                                data-te-target="#rating-petugas-layanan"
+                                data-te-nav-active
+                                role="tab"
+                                aria-controls="rating-petugas-layanan"
+                                aria-selected="true">
+                                Rating Petugas Layanan
+                            </a>
+                        </li>
+                        <li role="presentation">
+                            <a
+                                href="#rating-layanan"
+                                class="focus:border-transparen my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+                                data-te-toggle="pill"
+                                data-te-target="#rating-layanan"
+                                role="tab"
+                                aria-controls="rating-layanan"
+                                aria-selected="false">Rating Layanan
+                            </a>
+                        </li>
+                        <li role="presentation">
+                            <a
+                                href="#saran-pengaduan"
+                                class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+                                data-te-toggle="pill"
+                                data-te-target="#saran-pengaduan"
+                                role="tab"
+                                aria-controls="saran-pengaduan"
+                                aria-selected="false">
+                                Saran Pengaduan
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="year">
                         <select wire:model="selectedYear" data-te-select-init data-te-select-filter="true" data-te-select-size="lg">
                             <option value="null" hidden selected>Pilih Tahun ...</option>
                             @foreach ($years as $yearItem)
@@ -26,96 +57,106 @@
                         </select>
                         <label data-te-select-label-ref>Tahun</label>
                     </div>
-                    <button wire:click="resetData" class="btn-primary ml-4">
-                        @include('components.icon', ['name' => 'arrow-path', 'size' => 'w-5 h-5'])
-                    </button>
+                </div>
+                {{-- Content --}}
+                <div class="mb-6">
+                    {{-- Rating Petugas --}}
+                    <div
+                        class="hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                        id="rating-petugas-layanan"
+                        role="tabpanel"
+                        aria-labelledby="rating-petugas-layanan-tab"
+                        data-te-tab-active>
+                        <table class="w-full table-auto">
+                            <thead>
+                                <tr class="text-left font-bold bg-neutral-100">
+                                    @foreach ($officerRating[0] as $columnOfficer)
+                                        <th class="px-6 pt-6 pb-4">{{ $columnOfficer }}</th>
+                                    @endforeach
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($officerRating[1] as $reportOfficer)
+                                    <tr class="hover:bg-gray-200 focus-within:bg-grey-lightest">
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4 items-center">
+                                                {{ date('F', mktime(0, 0, 0, $reportOfficer->bulan, 10)) }}
+                                            </span>
+                                        </td>
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4">
+                                                {{ $reportOfficer->nama ?? '-' }}
+                                            </span>
+                                        </td>
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4 flex items-center">
+                                                {{ round($reportOfficer->rerata, 2) }}
+                                            </span>
+                                        </td>
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4">
+                                                {{ $reportOfficer->jumlah_terlayani }}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {{-- Rating Layanan --}}
+                    <div
+                        class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                        id="rating-layanan"
+                        role="tabpanel"
+                        aria-labelledby="rating-layanan-tab">
+                        <table class="w-full table-auto">
+                            <thead>
+                                <tr class="text-left font-bold bg-neutral-100">
+                                    @foreach ($serviceRating[0] as $columnService)
+                                        <th class="px-6 pt-6 pb-4">{{ $columnService }}</th>
+                                    @endforeach
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($serviceRating[1] as $reportService)
+                                    <tr class="hover:bg-gray-200 focus-within:bg-grey-lightest">
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4 items-center">
+                                                {{ date('F', mktime(0, 0, 0, $reportService->bulan, 10)) }}
+                                            </span>
+                                        </td>
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4">
+                                                {{ $reportService->nama_layanan ?? '-' }}
+                                            </span>
+                                        </td>
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4 flex items-center">
+                                                {{ round($reportService->rerata, 2) }}
+                                            </span>
+                                        </td>
+                                        <td class="border-t">
+                                            <span class="pl-6 py-4">
+                                                {{ $reportService->jumlah_terlayani }}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {{-- Saran Pengaduan --}}
+                    <div
+                        class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                        id="saran-pengaduan"
+                        role="tabpanel"
+                        aria-labelledby="rating-layanan-tab">
+                    Tab 3 content
+                    </div>
                 </div>
             </div>
-            {{ dd($monthlyReport) }}
-            @if ($monthlyReport->count() === 0)
-                <img src="{{ asset('files/404.svg') }}" class="w-full border-t">
-            @else
-                <table class="w-full table-auto">
-                    <thead>
-                        <tr class="text-left font-bold bg-neutral-100">
-                            @foreach ($monthlyReport[0] as $column)
-                                <th class="px-6 pt-6 pb-4">$column</th>
-                            @endforeach
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {{-- @foreach ($dailyReport->paginate(20) as $report)
-                            <tr class="hover:bg-gray-200 focus-within:bg-grey-lightest">
-                                <td class="border-t">
-                                    <span class="pl-6 py-4 items-center">
-                                        <i class="fas fa-calendar opacity-50 text-sm"></i> {{ DateFormat::convertDateTime($report->created_at) }}
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4">
-                                        {{ $report->petugas->nama ?? '-' }}
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4 flex items-center">
-                                        @if (!is_null($report->rating_petugas))
-                                            @for($i = 0; $i < 5; $i++)
-                                                @if($i < $report->rating_petugas)
-                                                    <i class="fas fa-star text-secondary-400"></i>
-                                                @else
-                                                    <i class="far fa-star text-secondary-400"></i>
-                                                @endif
-                                            @endfor
-                                        @else
-                                            -
-                                        @endif
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4">
-                                        {{ $report->layanan->nama_layanan }}
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4">
-                                        @for($i = 0; $i < 5; $i++)
-                                            @if($i < $report->rating_layanan)
-                                                <i class="fas fa-star text-secondary-400"></i>
-                                            @else
-                                                <i class="far fa-star text-secondary-400"></i>
-                                            @endif
-                                        @endfor
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4">
-                                        {{ $report->nama_konsumen }}
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4">
-                                        {{ Str::limit($report->saran_pengaduan, 50) }}
-                                    </span>
-                                </td>
-                                <td class="border-t">
-                                    <span class="pl-6 py-4">
-                                        @if(!is_null($report->kode_saran))
-                                            @for($i = 0; $i < count($report->kode_saran); $i++)
-                                                <div class="relative inline-block px-3 py-1 text-sm text-green-900 leading-tight">
-                                                    <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                                    <span class="relative">{{ \App\Models\m_saran::where('kode_saran', collect($report->kode_saran)->get($i))->pluck('nama_saran')[0] }}</span>
-                                                </div>
-                                            @endfor
-                                        @else
-                                            -
-                                        @endif
-                                    </span>
-                                </td>
-                            </tr>
-                        @endforeach --}}
-                    </tbody>
-                </table>
-            @endif
         </div>
     </section>
 </div>
