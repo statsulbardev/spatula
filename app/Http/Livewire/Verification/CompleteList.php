77f5913c57833dfe6f942a1b7f3f649b1d74e410
Syaifur Rijal <syaifur.rijal@hotmail.com>
2023-05-31 09:59:02 +0800

    Simpan Kode

--- app/Http/Livewire/Verification/CompleteList.php
+++ app/Http/Livewire/Verification/CompleteList.php
@@ -2,16 +2,42 @@
 
 namespace App\Http\Livewire\Verification;
 
+use App\Models\d_penilaian;
+use App\Traits\UnitCode;
 use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class CompleteList extends Component
 {
-    use WithPagination;
+    use UnitCode, WithPagination;
+
+    public int $numberOfPagination = 20;
+    public ?string $searchKeyword = null;
 
     public function render() : View
     {
-        return view('livewire.verification.complete-list')->layout('layouts.app');
+        return view('livewire.verification.complete-list', [
+            'dones' => $this->retrieveData()
+        ])->layout('layouts.app');
+    }
+
+    public function updatedNumberOfPagination()
+    {
+        $this->resetPage();
+    }
+
+    private function retrieveData()
+    {
+        $result = auth()->user()->hasRole('superadmin')
+            ? d_penilaian::search($this->searchKeyword)
+                -> where('selesai', 1)
+                -> orderBy('tanggal_selesai', 'desc')
+            : d_penilaian::search($this->searchKeyword)
+                -> where('kode_satker_id', $this->getUnitCode()->kode_satker)
+                -> where('selesai', 1)
+                -> orderBy('tanggal_selesai', 'desc');
+
+        return $result->paginate($this->numberOfPagination);
     }
 }
