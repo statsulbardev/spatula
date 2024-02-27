483f85137fa0a4bd9d741353857abfe440d0df5d
budi.akkas <budi.akkas@bps.go.id>
2024-02-27 11:37:49 +0800

    init yang lain

--- app/Http/Livewire/Antrian_Configuration/DaftarAntrianLihat.php
+++ app/Http/Livewire/Antrian_Configuration/DaftarAntrianLihat.php
@@ -7,7 +7,7 @@ use App\Traits\HasModelProcess;
 use Illuminate\View\View;
 use Livewire\Component;
 
-class Konfigurasi extends Component
+class DaftarAntrianLihat extends Component
 {
     use HasModelProcess;
 
