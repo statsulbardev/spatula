edb0c60737dcc9d60cd9795d8f9c9621c487cdbb
budi.akkas <budi.akkas@bps.go.id>
2024-03-10 09:07:29 +0800

    for prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         Model::shouldBeStrict(! $this->app->isProduction());
+
+        // URL::forceScheme('https');
     }
 }
