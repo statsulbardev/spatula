24d8f93f18cc8b3158de798748fe2f00a9284117
budi.akkas <budi.akkas@bps.go.id>
2024-03-14 04:02:05 +0800

    daftar

--- app/Livewire/Antrian/Admin/DaftarAntrian.php
+++ app/Livewire/Antrian/Admin/DaftarAntrian.php
@@ -4,7 +4,7 @@ namespace App\Livewire\Antrian\Admin;
 
 use App\Models\d_antrian_satker;
 use App\Models\m_antrian_satker_layanan;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -29,6 +29,9 @@ class DaftarAntrian extends Component
 
     public function render() : View
     {
+        $superadmin_role = auth()->user()->hasRole('superadmin');
+        $user_unit_code  = auth()->user()->satker->kode_satker;
+
         $master_antrian_satker = m_antrian_satker_layanan::all();
         $master_key_value = [];
         foreach($master_antrian_satker as $item){
@@ -39,7 +42,9 @@ class DaftarAntrian extends Component
         if($this->tanggal_filter){
             $data_to_render = d_antrian_satker::with(['satker', 'layanan'])
                 ->where('tanggal', $this->tanggal_filter)
-                ->where('kode_satker', Auth::user()->kode_satker_id)
+                ->when(!$superadmin_role, function (Builder $query, $data) use ($user_unit_code) {
+                    $query->where('kode_satker', $user_unit_code);
+                })
                 ->orderby('antrian', 'asc')
                 ->get();
         }
